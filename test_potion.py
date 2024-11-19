2e962010bb80896071671e54dc3199b8432a799f
Nick Sutton <sbnutton@gmail.com>
2024-11-18 17:04:57 -0800

    Fix unit test for potion

--- test_potion.py
+++ test_potion.py
@@ -1,11 +1,21 @@
 from unittest import TestCase
 from game import potion
-from game import make_monster
+import io
+from unittest.mock import patch
 
+class TestPotion(TestCase):
 
-class Test(TestCase):
-    def test_potion(self):
-        monster = make_monster()
-        actual = potion(monster)
-        expected = "Potion used! Monster's HP restored to max. Potions left: 2"
-        self.assertEqual(actual, expected)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_potion_with_uses_left(self, mock_stdout):
+        test_monster = {
+            'wins': 0,
+            'hp': 1,
+            'max_hp': 10,
+            'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
+            'x-coordinate': 0,
+            'y-coordinate': 0,
+            'potion_uses': 2
+        }
+        potion(test_monster)
+        expected_output = "Potion used! Monster's HP restored to max. Potions left: 1"
+        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
