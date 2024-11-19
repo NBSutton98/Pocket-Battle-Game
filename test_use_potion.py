a47c0f962f97ef810bd84058e05fe41759355bce
Nick Sutton <sbnutton@gmail.com>
2024-11-18 17:01:35 -0800

    Fix unit test for use_potion

--- test_use_potion.py
+++ test_use_potion.py
@@ -1,13 +1,16 @@
 from unittest import TestCase
 from game import use_potion
-from game import potion
+import io
 from unittest.mock import patch
+
 
 class Test(TestCase):
 
-    @patch('builtins.input', side_effect=['1'])
-    def test_use_potion(self, _):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['1'])  # Mocking input to simulate user entering '1'
+    def test_use_potion(self, mock_input, mock_stdout):
         test_monster = {'hp': 1, 'max_hp': 10, 'potion_uses': 3}
-        actual = use_potion(test_monster)
-        expected = "Monster wins!"
-        self.assertEqual(actual, expected)
+        use_potion(test_monster)
+        expected = "Potion used! Monster's HP restored to max. Potions left: 2"
+        self.assertEqual(mock_stdout.getvalue().strip(), expected)
+
