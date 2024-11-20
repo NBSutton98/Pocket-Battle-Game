60c04c3c49b0adb2168e37eb9d11d86134b9bfa5
Nick Sutton <sbnutton@gmail.com>
2024-11-19 17:15:32 -0800

    Add unit test to move_choice

---
+++ test_move_choice.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from game import move_choice
+import io
+from unittest.mock import patch
+
+
+class Test(TestCase):
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['1'])
+    def test_move_choice(self, mock_input, mock_stdout):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+                   'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
+                   'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
+        actual = move_choice(test_monster)
+        expected = 'ember'
+        self.assertEqual(expected, actual)
