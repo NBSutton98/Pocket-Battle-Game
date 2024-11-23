12b9990463548c54e8deaf852728f33c7699400b
Nick Sutton <sbnutton@gmail.com>
2024-11-23 10:40:15 -0800

    Add suite of unit tests to battle

--- test_battle.py
+++ test_battle.py
@@ -27,3 +27,25 @@ class TestBattle(TestCase):
         actual = battle(monster, enemy)
         expected = "Enemy wins!"
         self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['1', '1', '1'])
+    @patch('random.randint', return_value=50)
+    def test_battle_monster_wins_multiple_turns(self, mock_stdout, mock_input, mock_randint):
+        monster = make_monster()
+        enemy = make_enemy()
+        enemy['hp'] = 5
+        actual = battle(monster, enemy)
+        expected = "Monster wins!"
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['1', '1'])
+    @patch('random.randint', return_value=50)
+    def test_battle_monster_lose_multi_turns(self, mock_stdout, mock_input, mock_randint):
+        monster = make_monster()
+        enemy = {'hp': 50, 'max_hp': 100,
+                 'moves': {'bite': {'power': 10, 'accuracy': 90}, 'punch': {'power': 10, 'accuracy': 100}}}
+        actual = battle(monster, enemy)
+        expected = "Enemy wins!"
+        self.assertEqual(expected, actual)
