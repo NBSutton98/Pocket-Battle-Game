7371b5c15c25cd1303aa3e3c61fa41493683326c
Nick Sutton <sbnutton@gmail.com>
2024-11-23 11:10:20 -0800

    Add suite of unit tests to use_move

--- test_use_move.py
+++ test_use_move.py
@@ -5,7 +5,7 @@ from game import make_monster, make_enemy, use_move
 
 class TestUseMove(TestCase):
     @patch('random.randint', return_value=50)
-    def test_use_move(self, _):
+    def test_use_move_death(self, _):
         monster = make_monster()
         enemy = make_enemy()
         move = 'ember'
@@ -13,3 +13,34 @@ class TestUseMove(TestCase):
         enemy['hp'] -= damage
         use_move(monster, enemy, move)
         self.assertEqual(enemy['hp'], 0)
+
+    @patch('random.randint', return_value=50)
+    def test_use_move_2_death(self, _):
+        monster = make_monster()
+        enemy = make_enemy()
+        move = 'scratch'
+        damage = 5
+        enemy['hp'] -= damage
+        use_move(monster, enemy, move)
+        self.assertEqual(enemy['hp'], 0)
+
+    @patch('random.randint', return_value=50)
+    def test_use_move_alive(self, _):
+        monster = make_monster()
+        enemy = make_enemy()
+        move = 'scratch'
+        damage = 1
+        enemy['hp'] -= damage
+        use_move(monster, enemy, move)
+        self.assertEqual(enemy['hp'], 3)
+
+    @patch('random.randint', return_value=50)
+    def test_use_move_alive_second_move(self, _):
+        monster = make_monster()
+        enemy = make_enemy()
+        move = 'ember'
+        damage = 1
+        enemy['hp'] -= damage
+        use_move(monster, enemy, move)
+        self.assertEqual(enemy['hp'], 1)
+
