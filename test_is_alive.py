38e63467f0ad9457e2797e5de9af976955d7cbf1
Nick Sutton <sbnutton@gmail.com>
2024-11-23 10:46:53 -0800

    Add suite of unit tests to is_alive

--- test_is_alive.py
+++ test_is_alive.py
@@ -1,11 +1,26 @@
 from unittest import TestCase
-from game import make_monster
+
 from game import is_alive
+from game import make_monster
 
 
 class Test(TestCase):
     def test_is_alive(self):
         monster = make_monster()
+        actual = is_alive(monster)
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_is_alive_false(self):
+        monster = make_monster()
+        monster['hp'] = 0
+        actual = is_alive(monster)
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_is_alive_weak(self):
+        monster = make_monster()
+        monster['hp'] = 1
         actual = is_alive(monster)
         expected = True
         self.assertEqual(actual, expected)
