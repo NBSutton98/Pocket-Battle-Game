a3d238431aab1902806112764a1a24b71474d1ae
Nick Sutton <sbnutton@gmail.com>
2024-11-23 11:00:08 -0800

    Reformat move_choice for PEP8

--- test_move_choice.py
+++ test_move_choice.py
@@ -8,7 +8,7 @@ class Test(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['1'])
-    def test_move_choice(self, mock_input, mock_stdout):
+    def test_move_choice(self, _, __):
         test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
                         'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
                         'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
@@ -18,8 +18,8 @@ class Test(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['2'])
-    def test_move_choice_2(self, mock_input, mock_stdout):
-        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+    def test_move_choice_2(self, _, __):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 1,
                         'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
                         'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
         actual = move_choice(test_monster)
@@ -28,8 +28,8 @@ class Test(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['3', '2'])
-    def test_move_choice_invalid_number(self, mock_input, mock_stdout):
-        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+    def test_move_choice_invalid_number(self, _, __):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 3,
                         'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
                         'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
         actual = move_choice(test_monster)
@@ -38,8 +38,8 @@ class Test(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['w', '2'])
-    def test_move_choice_invalid_letter(self, mock_input, mock_stdout):
-        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+    def test_move_choice_invalid_letter(self, _, __):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 5,
                         'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
                         'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
         actual = move_choice(test_monster)
