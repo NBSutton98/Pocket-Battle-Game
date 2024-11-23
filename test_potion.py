4d853e8c68c7cc502814905fba8ef49b74a9e1f9
Nick Sutton <sbnutton@gmail.com>
2024-11-23 11:07:08 -0800

    Add suite of unit tests to potion

--- test_potion.py
+++ test_potion.py
@@ -1,52 +1,35 @@
-from unittest import TestCase
-from game import potion
 import io
+from unittest import TestCase
 from unittest.mock import patch
+
+from game import potion
 
 
 class TestPotion(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_potion_with_uses_left(self, mock_stdout):
-        test_monster = {
-            'wins': 0,
-            'hp': 1,
-            'max_hp': 10,
+        test_monster = {'wins': 0, 'hp': 1, 'max_hp': 10,
             'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
-            'x-coordinate': 0,
-            'y-coordinate': 0,
-            'potion_uses': 2
-        }
+            'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
         potion(test_monster)
         expected_output = "Potion used! Monster's HP restored to max. Potions left: 1"
         self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_potion_with_1_use_left(self, mock_stdout):
-        test_monster = {
-            'wins': 0,
-            'hp': 2,
-            'max_hp': 10,
+        test_monster = {'wins': 0, 'hp': 2, 'max_hp': 10,
             'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
-            'x-coordinate': 0,
-            'y-coordinate': 0,
-            'potion_uses': 1
-        }
+            'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 1}
         potion(test_monster)
         expected_output = "Potion used! Monster's HP restored to max. Potions left: 0"
         self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_potion_with_0_use_left(self, mock_stdout):
-        test_monster = {
-            'wins': 0,
-            'hp': 2,
-            'max_hp': 10,
+        test_monster = {'wins': 0, 'hp': 2, 'max_hp': 10,
             'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
-            'x-coordinate': 0,
-            'y-coordinate': 0,
-            'potion_uses': 0
-        }
+            'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 0}
         potion(test_monster)
         expected_output = "No potions left to use!"
         self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
