a45a2109f52a673b8f7da10bb7021e4334b41af8
Nick Sutton <sbnutton@gmail.com>
2024-11-23 10:58:12 -0800

    Add suite of unit tests to move_choice

--- test_move_choice.py
+++ test_move_choice.py
@@ -15,3 +15,33 @@ class Test(TestCase):
         actual = move_choice(test_monster)
         expected = 'ember'
         self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['2'])
+    def test_move_choice_2(self, mock_input, mock_stdout):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+                        'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
+                        'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
+        actual = move_choice(test_monster)
+        expected = 'scratch'
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['3', '2'])
+    def test_move_choice_invalid_number(self, mock_input, mock_stdout):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+                        'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
+                        'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
+        actual = move_choice(test_monster)
+        expected = 'scratch'
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=['w', '2'])
+    def test_move_choice_invalid_letter(self, mock_input, mock_stdout):
+        test_monster = {'wins': 0, 'hp': 5, 'max_hp': 10,
+                        'moves': {'ember': {'power': 5, 'accuracy': 80}, 'scratch': {'power': 3, 'accuracy': 100}},
+                        'x-coordinate': 0, 'y-coordinate': 0, 'potion_uses': 2}
+        actual = move_choice(test_monster)
+        expected = 'scratch'
+        self.assertEqual(expected, actual)
