e095b35caf0d6b276a4988de77aea4f8f14b190e
Nick Sutton <sbnutton@gmail.com>
2024-11-25 09:07:43 -0800

    Add suite of unit test to intro

---
+++ test_intro.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+from game import intro
+
+
+class TestIntro(TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_intro(self, mock_stdout):
+        expected = (
+            "Thank you for trying my first game, this will be a battle simulation featuring your very own monster, "
+            "your objective is, explore, get a total of 6 battle wins and defeat the final challenge, good luck! \n"
+            "Well, it is finally time....Go set off on your adventure, but make sure to bring that crazy monster of "
+            "yours for protection, umm... what was its name again?\n"
+        )
+        intro()
+        actual = mock_stdout.getvalue()
+        self.assertEqual(actual, expected)
+
+
