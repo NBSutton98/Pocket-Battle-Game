981544dfc977c59547c6a930e70f8836d010a8f6
Nick Sutton <sbnutton@gmail.com>
2024-11-25 06:30:00 -0800

    Add a suite of unit test to describe_current_location_test

---
+++ describe_current_location_test.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+import game
+from game import make_board
+
+
+class Test(TestCase):
+
+    @patch('random.choice', side_effect=["A cozy studio with a warm fireplace and vintage decor."])
+    def test_describe_current_location_start(self, _):
+        board = make_board(1, 1)
+        character = {"x-coordinate": 0, "y-coordinate": 0, "current_hp": 5}
+        actual = game.describe_current_location(board, character)
+        expected = "A cozy studio with a warm fireplace and vintage decor."
+        self.assertEqual(expected, actual)
+
+    @patch('random.choice', side_effect=["A colorful child's playroom filled with toys and whimsical murals.", "A spacious laundry room with organized shelves and a utility sink.",
+        "A cozy corner in the basement with a plush rug and soft lighting.", "A tranquil meditation room with soft cushions and calming decor."])
+    def test_describe_current_location_board_size(self, _):
+        board = make_board(2, 2)
+        character = {"x-coordinate": 1, "y-coordinate": 1, "current_hp": 5}
+        actual = game.describe_current_location(board, character)
+        expected = "A tranquil meditation room with soft cushions and calming decor."
+        self.assertEqual(expected, actual)
+
+    @patch('random.choice', side_effect=["A colorful child's playroom filled with toys and whimsical murals.", "A spacious laundry room with organized shelves and a utility sink.",
+        "A cozy corner in the basement with a plush rug and soft lighting.", "A tranquil meditation room with soft cushions and calming decor."])
+    def test_describe_current_location_character_position(self, _):
+        board = make_board(2, 2)
+        character = {"x-coordinate": 1, "y-coordinate": 1, "current_hp": 5}
+        actual = game.describe_current_location(board, character)
+        expected = "A tranquil meditation room with soft cushions and calming decor."
+        self.assertEqual(expected, actual)
+
+    @patch('random.choice', side_effect=["A colorful child's playroom filled with toys and whimsical murals.", "A spacious laundry room with organized shelves and a utility sink.",
+        "A cozy corner in the basement with a plush rug and soft lighting.", "A tranquil meditation room with soft cushions and calming decor."])
+    def test_describe_current_location_character_x_position(self, _):
+        board = make_board(2, 2)
+        character = {"x-coordinate": 0, "y-coordinate": 1, "current_hp": 5}
+        actual = game.describe_current_location(board, character)
+        expected = "A spacious laundry room with organized shelves and a utility sink."
+        self.assertEqual(expected, actual)
+
+
+
