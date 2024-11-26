7bf4cefefa51254cfbdfaec42f4bbf1c1b2822d5
Nick Sutton <sbnutton@gmail.com>
2024-11-26 14:21:57 -0800

    Reformat unit tests

--- test_final_battle.py
+++ test_final_battle.py
@@ -1,14 +1,16 @@
 from unittest import TestCase
 from unittest.mock import patch
 import io
-from game import final_battle, make_monster, make_enemy
+from battle import final_battle
+from characters import make_monster, make_enemy
 
 
 class TestBattle(TestCase):
+
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['1', '1'])
     @patch('random.randint', return_value=50)
-    def test_final_battle(self, _, __, ___):
+    def test_final_battle_win(self, _, __, ___):
         name = 'nick'
         monster = make_monster(name)
         enemy = make_enemy()
@@ -20,7 +22,7 @@ class TestBattle(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['1', '1'])
     @patch('random.randint', return_value=50)
-    def test_final_battle(self, _, __, ___):
+    def test_final_battle_lose(self, _, __, ___):
         name = 'nick'
         monster = make_monster(name)
         monster['hp'] = 1
--- test_make_board.py
+++ test_make_board.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
 from unittest.mock import patch
-
-import game
+import board
 
 
 class Test(TestCase):
 
     @patch('random.choice', side_effect=["A cozy studio with a warm fireplace and vintage decor."])
     def test_make_board_smallest(self, _):
-        actual = game.make_board(1, 1)
+        actual = board.make_board(1, 1)
         expected = {(0, 0): "A cozy studio with a warm fireplace and vintage decor."}
         self.assertEqual(actual, expected)
 
@@ -17,7 +16,7 @@ class Test(TestCase):
                                          "A cozy corner in the basement with a plush rug and soft lighting.",
                                          "A tranquil meditation room with soft cushions and calming decor."])
     def test_make_board_big(self, _):
-        actual = game.make_board(2, 2)
+        actual = board.make_board(2, 2)
         expected = {(0, 0): "A colorful child's playroom filled with toys and whimsical murals.",
                     (0, 1): 'A spacious laundry room with organized shelves and a utility sink.',
                     (1, 0): 'A cozy corner in the basement with a plush rug and soft lighting.',
