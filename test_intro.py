bd5597b29092d98047a0a0f29909088692c79402
Nick Sutton <sbnutton@gmail.com>
2024-11-27 12:35:37 -0800

    Adjust test_intro with new line

--- test_intro.py
+++ test_intro.py
@@ -9,10 +9,14 @@ class TestIntro(TestCase):
     def test_intro(self, mock_stdout):
         expected = (
             "Thank you for trying my first game, this will be a battle simulation featuring your very own monster, "
-            "your objective is, explore, get a total of 6 battle wins and defeat the final challenge, good luck! \n"
-            "Well, it is finally time....Go set off on your adventure, but make sure to bring that crazy monster of "
+            "your objective is, explore,\n"
+            "get a total of 6 battle wins and defeat the final challenge, good luck! \n\n"
+            "Well, it is finally time....Go set off on your adventure,\n"
+            "but make sure to bring that crazy monster of "
             "yours for protection, umm... what was its name again?\n"
         )
         intro()
         actual = mock_stdout.getvalue()
         self.assertEqual(actual, expected)
+
+
